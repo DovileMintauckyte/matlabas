c3ca3934947f63c7b484f42a0846b9cd3f714c7f
Dovile Mintauckyte <mintauckyte@gmail.com>
2013-03-28 12:28:30 +0200

    padareme dvimati montecarlo

---
+++ sincos2.m
@@ -0,0 +1,7 @@
+function fsincos2=sincos2(x);
+% grazina f-jos :f(x)=sin(x(1))+cos(x(2)) reiksme
+% paleidimas: f=sincos2(x)kur x vienmatis vektorius
+% pvz: f=sincos2([1.0, 1.0]);
+% Leistinoji sritis:[-10;10]^2
+fsincos2=sin(x(1))+cos(x(2));
+end
